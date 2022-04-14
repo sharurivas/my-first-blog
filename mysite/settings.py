c49f9d8b27adba7a51ac611fbfc0aa075f97964f
sharu.rivas <sharonrr510@gmail.com>
2022-04-14 16:35:54 -0300

    Modificado settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Argentina/Cordoba'
 
